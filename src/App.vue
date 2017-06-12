76102c7b5bb62858d845ea8d25afdbf52228e55f
wahaha2016 <zsx-125@163.com>
2017-06-12 08:51:46 +0800

    replace model

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  created: function () {
-              this.$router.replace('/login')
-          }
-}
+    export default{
+      name: 'APP'
+    }
 </script>
-
-<style>
-#app {
-    background: #18c8f6;
-    height: 100%;
-    background:url("./assets/img/login-background.jpg") no-repeat center fixed;
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-    background-size: cover;
-    color: rgba(255,255,255,.95);
-    padding:1px;
-}
-</style>
